d3a46d5d09b2f4ed36e6b8b53a34ca14e9b917fd
ArchiePrince <acheampongprince77@gmail.com>
2025-02-10 23:21:11 -0500

    Added task_3.sql to the repo

---
+++ task_3.sql
@@ -0,0 +1,7 @@
+-- # Title:  Create Your Magical Tables
+-- # Author: Archie Prince
+-- # Filename: task_3.sql
+-- # Date: 10.02.2025
+-- # Objective: A script that list all the tables in alx_book_store in your MySQL server.
+
+SHOW TABLES;
