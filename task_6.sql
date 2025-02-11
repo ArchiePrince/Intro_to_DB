c84b4fe19b64213867f13a350fc8f68ef03e3789
ArchiePrince <acheampongprince77@gmail.com>
2025-02-10 23:34:49 -0500

    Added task_6.sql to the repo

---
+++ task_6.sql
@@ -0,0 +1,11 @@
+-- # Title:  Populating Your Database with Your More Data
+-- # Author: Archie Prince
+-- # Filename: task_6.sql
+-- # Date: 11.02.2025
+-- # Objective: A script that inserts multiple rows in the table customer (database alx_book_store) in your MySQL server.
+
+INSERT INTO customers (customer_id, customer_name, email, address) 
+VALUES 
+    (2, 'Blessing Malik', 'bmalik@sandtech.com', '124 Happiness Ave.'),
+    (3, 'Obed Ehoneah', 'eobed@sandtech.com', '125 Happiness Ave.'),
+    (4, 'Nehemial Kamolu', 'nkamolu@sandtech.com', '126 Happiness Ave.');
