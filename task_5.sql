b9cb496c6ce0b37350b6bcbf39d401ae29b99be7
ArchiePrince <acheampongprince77@gmail.com>
2025-02-10 23:31:39 -0500

    Added task_5.sql to the repo

---
+++ task_5.sql
@@ -0,0 +1,9 @@
+-- # Title:  CFull description
+-- # Author: Archie Prince
+-- # Filename: task_5.sql
+-- # Date: 11.02.2025
+-- # Objective: A script that inserts a single row in the table customer (database alx_book_store) in your MySQL server.
+
+
+INSERT INTO customers (customer_id, customer_name, email, address) 
+VALUES (1, 'Cole Baidoo', 'cbaidoo@sandtech.com', '123 Happiness Ave.');
